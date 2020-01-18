4d6bd7720776838d80490c96ec50c0bf4a28b3b3
Kiran Randhawa <Kiran.Randhawa85@gmail.com>
2020-01-18 16:30:50 +0000

    Created user content controller

---
+++ UserPhotoContent.Api/Controllers/UserPhotoContentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace UserPhotoContent.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserPhotoContentController : ControllerBase
+    {
+        private readonly ILogger<UserPhotoContentController> _logger;
+
+        public UserPhotoContentController(ILogger<UserPhotoContentController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
