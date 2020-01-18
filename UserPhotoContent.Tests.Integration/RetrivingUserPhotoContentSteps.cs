1179d08a5ba09fcaccb34ec971b2edf2b5fb3f90
Kiran Randhawa <Kiran.Randhawa85@gmail.com>
2020-01-18 14:53:12 +0000

    Generated Specflow step definition scaffold.

---
+++ UserPhotoContent.Tests.Integration/RetrivingUserPhotoContentSteps.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace UserPhotoContent.Tests.Integration
+{
+    [Binding]
+    public class RetrivingUserPhotoContentSteps
+    {
+        [Given(@"The user does not have any content")]
+        public void GivenTheUserDoesNotHaveAnyContent()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have (.*) and (.*)")]
+        public void GivenIHaveAnd(int p0, int p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the data is requested in ""(.*)"" format")]
+        public void WhenTheDataIsRequestedInFormat(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the data is requested (.*) format")]
+        public void WhenTheDataIsRequestedFormat(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the resulting HTTP Status code should be (.*)")]
+        public void ThenTheResultingHTTPStatusCodeShouldBe(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the HTTP Status code should be (.*)")]
+        public void ThenTheHTTPStatusCodeShouldBe(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should contain (.*) photos")]
+        public void ThenTheResultShouldContainPhotos(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should contain (.*) albums")]
+        public void ThenTheResultShouldContainAlbums(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"The content should be formatted as (.*)")]
+        public void ThenTheContentShouldBeFormattedAs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
